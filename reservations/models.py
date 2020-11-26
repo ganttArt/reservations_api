9a06ae8724e77291da653ed24f46dd7406d4895b
Chris Gantt <gantt.art@gmail.com>
2020-11-25 17:35:13 -0800

    Models created and Admin registered.

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Table(models.Model):
+    description = models.CharField(max_length=100, unique=True)
+    capacity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.description
+
+
+class Reservation(models.Model):
+    table = models.ForeignKey(Table, on_delete=models.PROTECT)
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.date) + ' ' + self.table.description
+    
+    class Meta:
+        unique_together = ['table', 'date']
